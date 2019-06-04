1d3909ebc7773c429f9fde80ad83a97785b0b11c
Jason <1393983569@qq.com>
2019-06-04 09:59:23 +0800

    first commit

---
+++ src/permission.js
@@ -0,0 +1,83 @@
+import router from './router'
+import store from './store'
+import { Message } from 'element-ui'
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
+import { getToken } from '@/utils/auth' // get token from cookie
+import getPageTitle from '@/utils/get-page-title'
+
+NProgress.configure({ showSpinner: false }) // NProgress Configuration
+
+const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
+
+router.beforeEach(async(to, from, next) => {
+  // start progress bar
+  NProgress.start()
+
+  // set page title
+  document.title = getPageTitle(to.meta.title)
+
+  // determine whether the user has logged in
+  const hasToken = getToken()
+
+  if (hasToken) {
+    if (to.path === '/login') {
+      // 如果已登录，则重定向到主页
+      // 注意:角色必须是一个对象数组! such as: ['admin'] or ,['developer','editor']
+      const { roles } = await store.dispatch('user/getInfo')
+      // 根据角色生成可访问路由映射
+      const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+      // 动态添加可访问路由
+      router.addRoutes(accessRoutes)
+      next({ path: '/' })
+      NProgress.done()
+    } else {
+      // 确定用户是否通过getInfo获得了他的权限角色
+      const hasRoles = store.getters.roles && store.getters.roles.length > 0
+      if (hasRoles) {
+        // 注意:角色必须是一个对象数组! such as: ['admin'] or ,['developer','editor']
+        const { roles } = await store.dispatch('user/getInfo')
+        // 根据角色生成可访问路由映射
+        const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+        // 动态添加可访问路由
+        router.addRoutes(accessRoutes)
+        next()
+      } else {
+        try {
+          // 获取用户信息
+          // 注意:角色必须是一个对象数组! such as: ['admin'] or ,['developer','editor']
+          const { roles } = await store.dispatch('user/getInfo')
+          // 根据角色生成可访问路由映射
+          const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+          // 动态添加可访问路由
+          router.addRoutes(accessRoutes)
+          // hack method to ensure that addRoutes is complete
+          // set the replace: true, so the navigation will not leave a history record
+          next({ ...to, replace: true })
+        } catch (error) {
+          // remove token and go to login page to re-login
+          await store.dispatch('user/resetToken')
+          Message.error(error || 'Has Error')
+          next(`/login?redirect=${to.path}`)
+          NProgress.done()
+        }
+      }
+    }
+  } else {
+    /* has no token*/
+
+    if (whiteList.indexOf(to.path) !== -1) {
+      // in the free login whitelist, go directly
+      next()
+    } else {
+      // other pages that do not have permission to access are redirected to the login page.
+      next(`/login?redirect=${to.path}`)
+      NProgress.done()
+    }
+  }
+})
+
+router.afterEach(() => {
+  // finish progress bar
+  NProgress.done()
+})
---
+++ src/store/modules/permission.js
@@ -0,0 +1,73 @@
+import { asyncRoutes, constantRoutes } from '@/router'
+
+/**
+ * 角色确定当前用户是否具有权限
+ * @param roles
+ * @param route
+ */
+function hasPermission(roles, route) {
+  // 判断是否设置权限
+  if (route.meta && route.meta.roles) {
+    // 判断如果route.meta.roles里包含role里的权限 就返回true
+    return roles.some(role => route.meta.roles.includes(role))
+  } else {
+    return true
+  }
+}
+
+/**
+ * 通过递归过滤异步路由表
+ * @param routes asyncRoutes
+ * @param roles
+ */
+export function filterAsyncRoutes(routes, roles) {
+  const res = []
+
+  routes.forEach(route => {
+    const tmp = { ...route }
+    if (hasPermission(roles, tmp)) {
+      if (tmp.children) {
+        tmp.children = filterAsyncRoutes(tmp.children, roles)
+      }
+      res.push(tmp)
+    }
+  })
+  return res
+}
+
+const state = {
+  routes: [],
+  addRoutes: []
+}
+
+const mutations = {
+  SET_ROUTES: (state, routes) => {
+    state.addRoutes = routes
+    state.routes = constantRoutes.concat(routes)
+  }
+}
+
+const actions = {
+  // 生成路由
+  generateRoutes({ commit }, roles) {
+    console.log(roles)
+    return new Promise(resolve => {
+      let accessedRoutes
+      if (roles.includes('editor')) {
+        // asyncRoutes 应该能从服务器获得
+        accessedRoutes = asyncRoutes || []
+      } else {
+        accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
+      }
+      commit('SET_ROUTES', accessedRoutes)
+      resolve(accessedRoutes)
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
