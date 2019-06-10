e064aff4246200222046198a352d238c330495cb
wang-zhenzhen <1332545292@qq.com>
2019-06-10 15:44:04 +0800

    shop.js

--- src/api/shop.js
+++ src/api/shop.js
@@ -1,10 +1,14 @@
 import request from '../utils/request'
 
+/**
+ * 添加店铺
+ * @param {Object} data 店铺信息对象
+ */
 export const addShop = (data) => {
   const params = new URLSearchParams()
   for (const key in data) {
-    console.log(key, 'key....')
-    // if (data[key]) params.append(key, data[key])
+    // console.log(key, 'key....')
+    if (data[key]) params.append(key, data[key])
   }
   return request({
     url: '/admin/shop/add',
@@ -12,27 +16,20 @@ export const addShop = (data) => {
     data: params
   })
 }
-// export const editShop = (data) => {
-//   const params = new URLSearchParams()
-//   // params.append('id', data.id)
-//   // params.append('name', data.name)
-//   // params.append('simpleName', data.simpleName)
-//   // params.append('imge', data.imge)
-//   // params.append('shopownerName', data.shopownerName)
-//   // params.append('shopownerPhone', data.shopownerPhone)
-//   // params.append('shopownerPassword', data.shopownerPassword)
-//   // params.append('cityId', data.cityId)
-//   // params.append('countyId', data.countyId)
-//   // params.append('detailsAddress', data.detailsAddress)
-//   // params.append('area', data.area)
-//   // params.append('idcategoryOneId', data.idcategoryOneId)
-//   // params.append('categoryTwoId', data.categoryTwoId)
-//   // params.append('management', data.management)
-//   // params.append(' status', data. status)
-//   // params.append('deleteStatus', data.deleteStatus)
-//   // return request({
-//     url: '/admin/shop/update',
-//     method: 'get',
-//     data: params
-//   })
-// }
+/**
+ * 编辑店铺信息
+ * @param {Object} data 店铺信息对象
+ */
+export const editShop = (data) => {
+  const params = new URLSearchParams()
+  for (const key in data) {
+    if (data[key]) {
+      params.append(key, data[key])
+    }
+  }
+  return request({
+    url: '/admin/shop/update',
+    method: 'get',
+    data: params
+  })
+}
