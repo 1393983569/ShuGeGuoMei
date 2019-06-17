836b17461b2c29d76ea388369227de57ce85ba55
wang-zhenzhen <1332545292@qq.com>
2019-06-17 18:38:21 +0800

    shopManage

--- src/components/selectorAddress/selectorAddress.vue
+++ src/components/selectorAddress/selectorAddress.vue
@@ -9,21 +9,38 @@
       <el-option v-for="item in cityList" :key="item.id" :value="item.id" :label="item.name" />
     </el-select>
     <span class="size-color font-weight">区/县:</span>
-    <el-select v-model="AreaId" style="width:90px;font-size:18px;color:#6e7b99;">
-      <el-option v-for="item in AreaList" :key="item.id" :value="item.id" :label="item.name" />
+    <el-select v-model="countyId" style="width:90px;font-size:18px;color:#6e7b99;">
+      <el-option v-for="item in countyList" :key="item.id" :value="item.id" :label="item.name" />
     </el-select>
   </div>
 </template>
 <script>
 import { getProvince, getCity, getArea } from '@/api/priovinCityArea.js'
 export default {
+  props: {
+    province1Id: {
+      type: String,
+      default: '',
+      require: true
+    },
+    city1Id: {
+      type: String,
+      default: '',
+      require: true
+    },
+    county1Id: {
+      type: String,
+      default: '',
+      require: true
+    }
+  },
   data() {
     return {
       provinceId: '',
       cityId: '',
-      AreaId: '',
+      countyId: '',
       cityList: [],
-      AreaList: [],
+      countyList: [],
       provinceList: []
     }
   },
@@ -46,7 +63,7 @@ export default {
       this.$emit('getCity', this.cityId)
       getArea(this.cityId).then(res => {
         if (res.status === 1) {
-          this.AreaList = res.info
+          this.countyList = res.info
         } else {
           this.$message.error('查询区县失败！')
         }
@@ -54,18 +71,26 @@ export default {
         this.$message.error(err)
       })
     },
-    'countryId'(e) {
-      this.countryId = e
-      this.$emit('getCountry', this.countryId)
+    'countyId'(e) {
+      this.countyId = e
+      this.$emit('getCounty', this.countyId)
     }
   },
   mounted() {
     this.getProvinceList()
+    if (this.province1Id) {
+      this.provinceId = this.province1Id
+    }
+    if (this.city1Id) {
+      this.cityId = this.city1Id
+    }
+    if (this.county1Id) {
+      this.countyId = this.county1Id
+    }
   },
   methods: {
     getProvinceList() {
       getProvince().then(res => {
-        // console.log(res, 'res....')
         if (res.status === 1) {
           this.provinceList = res.info
         } else {
--- src/views/shopManage/shop.vue
+++ src/views/shopManage/shop.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="body-margin">
     <div style="display:float;flex-direction: row;align-items: center;">
-      <selectorAddress @getProvince="getProvince" @getCity="getCity" @getCountry="getCountry" />
+      <selectorAddress @getProvince="getProvince" @getCity="getCity" @getCounty="getCounty" />
       <span class="item">经营模式:</span>
       <el-select v-model="management" style="width:100px;">
         <el-option v-for="item in managementList" :key="item.id" :value="item.id" :label="item.name" />
@@ -11,8 +11,8 @@
         <el-option v-for="item in orderList" :key="item.id" :value="item.id" :label="item.name" />
       </el-select>
       <div style="float:right;">
-        <el-button>筛选</el-button>
-        <el-button>清空</el-button>
+        <el-button @click="handleSearch">筛选</el-button>
+        <el-button @click="handleClearCondition">清空</el-button>
         <el-button @click="handleAdd">新建</el-button>
       </div>
     </div>
@@ -28,14 +28,15 @@
         <el-table-column prop="id" label="店铺ID" width="200px" />
         <el-table-column prop="simpleName" label="店铺简称" />
         <el-table-column prop="name" label="店铺名称" />
-        <!-- <el-table-column prop="vipNum" label="会员数" /> -->
+        <el-table-column prop="vipNum" label="会员数" />
         <el-table-column prop="management" label="经营模式" />
         <el-table-column prop="shopownerName" label="掌柜姓名" />
         <!-- <el-table-column prop="employeeNum" label="职员人数" /> -->
         <el-table-column prop="operate" label="操作" width="300px">
           <template slot-scope="scope">
             <el-button size="mini" @click="handleEdit(scope.row)">编辑</el-button>
-            <el-button size="mini" @click="handleStart(scope.row)">启用</el-button>
+            <el-button v-if="scope.row.status === 1" size="mini" @click="handleStart(scope.row)">启用</el-button>
+            <el-button v-else size="mini" @click="handleStart(scope.row)">停用</el-button>
             <el-button size="mini" @click="handleDelete(scope.row)">删除</el-button>
             <el-button size="mini" @click="handleDetail(scope.row)">详情</el-button>
           </template>
@@ -43,7 +44,7 @@
       </el-table>
     </div>
     <!-- 店铺编辑 -->
-    <shop-edit :show-edit="showEdit" :show-state="showState" :dialog-title="dialogTitle" @isClose="isClose" />
+    <shop-edit :show-edit="showEdit" :show-state="showState" :dialog-title="dialogTitle" :edit-object="editObject" @isClose="isClose" />
     <!-- 店铺详情 -->
     <shop-detail :show-detail="showDetail" :shop-object="shopObject" @isCloseDetail="isCloseDetail" />
     <el-dialog :visible.sync="showDelete" center width="380px" title="删除商品" style="border-ra">
@@ -57,29 +58,34 @@
       <div width="100%" style="font-size: 17px;display: flex;justify-content:center;align-items: center;height:100px;border-radius: 10px;">是否删除该商品？</div>
       <div slot="footer" style="boeder:1px solid black">
         <el-button style="width:160px;border:none;font-size:18px;" @click="showDelete = false">取消</el-button>
-        <el-button style="width:160px;border:none;font-size:18px;">确定</el-button>
+        <el-button style="width:160px;border:none;font-size:18px;" @click="confirmDelete">确定</el-button>
       </div>
     </el-dialog>
     <el-dialog :visible.sync="showStart" center width="380px" title="启用店铺" style="border-ra">
       <div width="100%" style="font-size: 17px;display: flex;justify-content:center;align-items: center;height:100px;border-radius: 10px;">是否启用该店铺？</div>
       <div slot="footer" style="boeder:1px solid black">
         <el-button style="width:160px;border:none;font-size:18px;" @click="showStart = false">取消</el-button>
-        <el-button style="width:160px;border:none;font-size:18px;">确定</el-button>
+        <el-button style="width:160px;border:none;font-size:18px;" @click="confirmStart">确定</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 <script>
+// import { Message } from 'element-ui'
 import selectorAddress from '@/components/selectorAddress/selectorAddress.vue'
 import shopEdit from './shopEidt.vue'
 import shopDetail from './shopDetail.vue'
-import { getShopList } from '@/api/shop.js'
+import { getShopList, deleteShop, startShop } from '@/api/shop.js'
+// import { sync } from 'glob'
 export default {
   components: {
     selectorAddress, shopEdit, shopDetail
   },
   data() {
     return {
+      // 店铺属性
+      id: 0,
+      name: '',
       showEdit: false,
       showState: false,
       showDelete: false,
@@ -88,7 +94,20 @@ export default {
       shopTable: [],
       management: '',
       orderId: '',
-      orderList: [],
+      // 启动
+      status: 1,
+      // 排序
+      orderList: [
+        {
+          id: 1,
+          name: '销售额'
+        },
+        {
+          id: 2,
+          name: '会员数'
+        }
+      ],
+      // 经营模式
       managementList: [
         {
           id: 1,
@@ -104,23 +123,25 @@ export default {
         }
       ],
       dialogTitle: '',
-      provinceId: '',
-      cityId: '',
-      countryId: '',
       pageNum: 1,
       pageSize: 10,
-      shopObject: {}
+      shopObject: {},
+      // 店铺编辑对象
+      editObject: {}
     }
   },
+  watch: {},
   mounted() {
     this.getShopList()
   },
   methods: {
     // 查询店铺列表
     getShopList() {
+      this.shopTable = []
       getShopList(this.pageNum, this.pageSize).then(res => {
         // console.log(res, 'jjjjjjjjjj')
         if (res.status === 1) {
+          // this.$message.success('启用成功！')
           res.info.records.map(e => {
             // console.log(e, 'gggggg')
             if (e.management === 1) {
@@ -150,16 +171,43 @@ export default {
       this.showState = true
       this.dialogTitle = '新建'
     },
+    // 删除店铺
     handleDelete(row) {
       this.showDelete = true
+      this.id = row.id
     },
+    async confirmDelete() {
+      const res = deleteShop(this.id)
+      if (res.status === 1) {
+        this.$message.success('删除成功！')
+      } else {
+        this.$message.error(res.info)
+      }
+    },
+    // 启用店铺
     handleStart(row) {
+      console.log(row, 'row,,,,,,')
       this.showStart = true
+      this.id = row.id
+      this.name = row.name
+    },
+    // 确定启用店铺
+    async confirmStart() {
+      this.status = 0
+      const res = await startShop(this.id, this.status)
+      if (res.status === 1) {
+        this.$message.success('启用成功！')
+        this.showStart = false
+        this.getShopList()
+      } else {
+        this.$message.error(res.info)
+      }
     },
     handleEdit(row) {
       this.showEdit = true
       this.showState = false
       this.dialogTitle = '编辑'
+      this.editObject = row
       // this.$router.push({ path:'/'})
     },
     handleDetail(row) {
@@ -177,12 +225,22 @@ export default {
     getCity(e) {
       this.cityId = e
     },
-    getCountry(e) {
-      this.countryId = e
+    getCounty(e) {
+      this.countyId = e
     },
     isClose(e) {
       this.showEdit = e
-    }
+    },
+    // 清空筛选条件
+    handleClearCondition() {
+      this.provinceId = ''
+      this.cityId = ''
+      this.countyId = ''
+      this.management = ''
+      this.orderId = ''
+    },
+    // 条件查询
+    handleSearch() {}
   }
 }
 </script>
